6899384deb594574382f35fefaa0d9706f86e38b
diogoandreteixeira <diogoteix1994@gmail.com>
2022-01-28 14:46:11 +0000

    solved exercice 4

--- homeWork.js
+++ homeWork.js
@@ -67,7 +67,16 @@ const crazyDiff = math.abs(a, 19)
  Write a function called "boundary" which accept an integer parameter n and returns true if n is within 20 and 100 (included) or if n it's equal to 400.
 */
 
-/* WRITE YOUR ANSWER HERE */
+const boundary = function(n) {
+    if (n >= 20 && n <= 100 || n === 400) {
+        console.log(true)
+    } else {
+        return false
+    }
+}
+
+let boundaryResult = boundary(400)
+console.log(boundaryResult)
 
 /* EXERCISE 5
  Write a function called "strivify" which accepts a string as a parameter.
